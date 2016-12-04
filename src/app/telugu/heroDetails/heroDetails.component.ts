da24b84e2349ed200e738d0f914255299fd1d9ab
sricharan <sricharan@MacBook-Pro.local>
2016-12-04 17:22:48 +0530

    added telugu subpages

---
+++ src/app/telugu/heroDetails/heroDetails.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector:'heroDetails',
+	templateUrl:'./heroDetails.html'
+})
+
+export class HeroDetailsComponent{
+
+}
---
+++ src/app/telugu/heroDetails/index.ts
@@ -0,0 +1,1 @@
+export * from './heroDetails.component'
