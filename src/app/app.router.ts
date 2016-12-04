20601ce3a2624d39b22be68a15375cd5626d8994
sricharan <sricharan@MacBook-Pro.local>
2016-12-04 17:22:26 +0530

    added telugu subpages

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -1,9 +1,23 @@
 import { Routes,RouterModule } from '@angular/router';
 import { TeleuguIndustryHerosListComponent } from './telugu'
+import { HeroDetailsComponent } from './telugu/heroDetails'
 
 const routes:Routes=[
 	{ path:'', redirectTo:'Telugu',pathMatch:'full'},
-	{ path:'Telugu',component:TeleuguIndustryHerosListComponent},
+	{ 	
+		path:'Telugu',
+		
+		children:[
+			{
+				path:'',
+				component:TeleuguIndustryHerosListComponent,
+			},
+			{
+				path:':heroId',
+				component:HeroDetailsComponent
+			}
+		]
+	},
 	{ path:'Tamil',loadChildren:'./tamil/tamil.module'},
 	{ path:'Hindi',loadChildren:'./hindi/hindi.module'},
 	{ path:'**', redirectTo:'/',pathMatch:'full'}
@@ -13,5 +27,6 @@ export const Routing =RouterModule.forRoot(routes,{useHash:false})
 
 
 export const RoutingComponents=[
-	TeleuguIndustryHerosListComponent
+	TeleuguIndustryHerosListComponent,
+	HeroDetailsComponent
 ]
